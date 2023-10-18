599371e923b2ab5bffb3de207727e9b1910dc6c9
RaulU <lraulchuy@gmail.com>
2023-10-18 15:18:39 -0600

    Ingreso de videos Inicio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,16 @@
             <h1 style="font-size: 2rem"><strong>SISTEMA DE SERVICIOS TECNICOS - SISTEC</strong></h1>
         </div>
         <div class="col s12 center-align">
-            <h3>UN VIDEO DE PRESENTACION DEL PROYECTO - PILOTO</h3>
+            <h3>VIDEO INTRODUCCION AL SISTEMA</h3>
+        </div>
+        <div class="col s12 center-align">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/SY4ySoewmDE?si=Rz9LTQRE6qLDEthP" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+        <div class="col s12 center-align">
+            <h3>VIDEO GUIA DE USO CODIGO QR</h3>
+        </div>
+        <div class="col s12 center-align">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/uxBhyt1vqww?si=LYa2FPOtyIj_xdec" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     </div>
 
