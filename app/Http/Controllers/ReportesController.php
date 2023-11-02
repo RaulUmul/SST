bc0689695abaadb7d0b69c9027f6af79c3041b14
RaulU <lraulchuy@gmail.com>
2023-11-02 16:41:55 -0600

    Cambios el 2/11/2023

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -4,23 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Servicio;
+use App\Models\User;
 
 class ReportesController extends Controller
 {
     //
     public function index(){
-        return view('reportes.index');
+
+        $tecnicos = User::whereJsonContains('roles',3)->get();
+
+
+        return view('reportes.index',compact('tecnicos'));
     }
 
     public function servicioPromedio(Request $request){
+
+
+        $tecnico_asignado = $request->tecnico;
+
         $serviciosCorrectivos = Servicio::whereNotNull('fecha_inicio')
         ->whereNotNull('fecha_finalizacion')
-        ->where('id_tipo_servicio',12)
-        ->get();
+        ->where('id_tipo_servicio',12);
+
+        if($tecnico_asignado != null) 
+            $serviciosCorrectivos = $serviciosCorrectivos->where('id_tecnico_asignado',$tecnico_asignado);
+
+        $serviciosCorrectivos = $serviciosCorrectivos->get();
+
         $serviciosPreventivos = Servicio::whereNotNull('fecha_inicio')
         ->whereNotNull('fecha_finalizacion')
-        ->where('id_tipo_servicio',11)
-        ->get();
+        ->where('id_tipo_servicio',11);
+        
+        if($tecnico_asignado != null) 
+            $serviciosPreventivos = $serviciosPreventivos->where('id_tecnico_asignado',$tecnico_asignado);
+
+        $serviciosPreventivos = $serviciosPreventivos->get();
+        
         return ['correctivos'=>$serviciosCorrectivos,'preventivos'=>$serviciosPreventivos];
     }
 }
--- resources/views/reportes/index.blade.php
+++ resources/views/reportes/index.blade.php
@@ -1,14 +1,34 @@
 @extends('layouts.plantilla')
 
 @section('content')
-<div>
-    <canvas id="myChart"></canvas>
+
+<div class="row">
+  <div class="col s12 m6">
+    <canvas id="myChartPromedio"></canvas>
+    <div class="input-field col s12">
+      <select id="tecnico_promedio">
+        <option value="{{null}}" disabled selected>Tecnico</option>
+        @foreach ($tecnicos as $key => $value)
+        <option value="{{$value->id_usuario}}">{{$value->nombres.' '.$value->apellidos}}</option>
+        @endforeach
+      </select>
+      <label>Filtrar por Tecnico</label>
+    </div>
   </div>
+  <div class="col s12 m6">
+    <canvas id="myChartTrabajados"></canvas>
+  </div>
+</div>
+
 @endsection
 
 @push('scripts')
 <script>
-    const ctx = document.getElementById('myChart');
+    $('select').formSelect();
+    let myChartPromedio;
+    let myChartTrabajados;
+    const ctx = document.getElementById('myChartPromedio');
+    const ctx_trabajados = document.getElementById('myChartTrabajados');
 
     // Seccion de Consulta de datos.
 
@@ -49,12 +69,53 @@
         }
     });
 
+    $('#tecnico_promedio').on('change',function(){
+      let tecnico = $(this).val();
+      myChart.destroy();
+      $.ajax({
+        type: 'get',
+        url: "{{route('reporte.servicioPromedio')}}",
+        data: {tecnico}, //Aqui enviamos segun el select
+        dataType: 'text',
+        success: function (response) {
+            // Correctivo
+            console.log((JSON.parse(response)).correctivos);
+            let correctivos = (JSON.parse(response)).correctivos;
+            let segundos_correctivos = 0;
+            let promedio_minutos_correctivo = 0;
+            let total_segundos_correctivo = 0;
+            // Preventivo
+            let preventivos = (JSON.parse(response)).preventivos;
+            let segundos_preventivos = 0;
+            let total_segundos_preventivos = 0;
+            let promedio_minutos_preventivos = 0;
+
+            correctivos.forEach(element => {
+                segundos_correctivos += Math.abs(Math.floor(Date.parse(element.fecha_inicio) / 1000) - Math.floor(Date.parse(element.fecha_finalizacion) / 1000));
+            });
+            total_segundos_correctivo = (Math.abs(segundos_correctivos)/60); //Pasado a minutos
+            promedio_minutos_correctivo = total_segundos_correctivo / correctivos.length; 
+
+            preventivos.forEach(element => {
+                segundos_preventivos += Math.abs(Math.floor(Date.parse(element.fecha_inicio) / 1000) - Math.floor(Date.parse(element.fecha_finalizacion) / 1000));
+            });
+            total_segundos_preventivos = (Math.abs(segundos_preventivos)/60); //Pasado a minutos
+            promedio_minutos_preventivos = total_segundos_preventivos / preventivos.length;
+
+            console.log('Minutos_correctivo',promedio_minutos_correctivo);
+            console.log('Minutos_preventivos',promedio_minutos_preventivos);
+
+            mantenimientoPromedio(promedio_minutos_preventivos,promedio_minutos_correctivo);
+        }
+      });
+    })
+
 
 
     // Esto estara en una funcion que renderizara cada que lo invoquen con los datos necesarios. 
 
     function mantenimientoPromedio(promPreventivo,promCorrectivo){
-        new Chart(ctx, {
+       myChartPromedio =  new Chart(ctx, {
           type: 'bar',
           data: {
             labels: ['Preventivo','Correctivo'], //Etiquetas (de cada columna)
@@ -72,6 +133,36 @@
             }
           }
         });
+    }
+   totalTrabajados()
+    function totalTrabajados(){
+      myChartTrabajados = new Chart(ctx_trabajados,{
+        type: 'line',
+        data:{
+          labels: ['Enero','Febrero','Marzo'],
+          datasets: [{
+              label: 'General',
+              data: [{x: 'Febrero',y:20},{x: 'Marzo',y:61}]
+            }]
+          },
+        options: {
+          scales: {
+              y: {
+                beginAtZero: true
+              }
+            },
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Servicios realizados'
+            }
+          }
+        },
+      })
     }
   </script>
 @endpush
